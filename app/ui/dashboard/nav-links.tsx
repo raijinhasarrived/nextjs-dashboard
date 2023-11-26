7252d4d6d444810176ad9434a439e78cab2e6afd
raijinhasarrived <raijinhasarrived@gmail.com>
2023-11-26 13:03:04 +0500

    ☄️ chapter 12 completed

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -32,9 +32,9 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-zinc-400 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'pointer-events-none bg-blue-700 text-white':
+                'pointer-events-none bg-zinc-900 text-white':
                   pathname === link.href,
               },
             )}
